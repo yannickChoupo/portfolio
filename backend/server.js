56c070e8a8cbc32f7d55bc0bde88cee7ed8e7afc
y.choupo <yannicknjilo@gmail.com>
2021-06-24 18:05:25 +0200

    v4

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,9 @@ app.use('/account/delete', deleteAccountRouter);
 if(process.env.NODE_ENV === "production") {
     // Set static folder
     app.use(express.static(path.join(__dirname, "../client/build")));
-    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname,'/../client/build/index.html')));
+    app.get("*", function (req, res) {
+        res.sendFile(path.resolve(__dirname,'../client/build', 'index.html'))
+    });
 }
 
 
