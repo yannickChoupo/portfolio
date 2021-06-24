23223c06bf31ddf73785d141814a3723cb7c50a5
y.choupo <yannicknjilo@gmail.com>
2021-06-24 05:42:50 +0200

    v1

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,7 @@ app.get('/',(req,res) => {
 if(process.env.NODE_ENV === "production") {
     // Set static folder
     app.use(express.static(path.join(__dirname, "../client/build")));
-    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname,'client','build','index.html')));
+    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname,'/../client/build/index.html')));
 }
 
 
