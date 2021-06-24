c11a28493705da23068d924266e3af1685da3e8d
y.choupo <yannicknjilo@gmail.com>
2021-06-24 04:49:45 +0200

    V3

--- backend/server.js
+++ backend/server.js
@@ -48,7 +48,7 @@ app.get('/',(req,res) => {
 // Serve static asssets if we are in production
 if(process.env.NODE_ENV === "production") {
     // Set static folder
-    app.use(express.static(path.join("/client/build")));
+    app.use(express.static(path.join(__dirname, "../client/build")));
     app.get("*", (req, res) => res.sendFile(path.resolve(__dirname,'client','build','index.html')));
 }
 
