e32131cbccc8b7d3e250477edf127309f3bd2c33
y.choupo <yannicknjilo@gmail.com>
2021-07-05 05:00:51 +0200

    fixing mobile device bug

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware,compose} from 'redux';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './redux/reducers';
 
 const initialState = {};
