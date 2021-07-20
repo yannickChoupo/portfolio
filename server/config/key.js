6938fa02a7edbfb29779635d69323e7ded95a792
y.choupo <yannicknjilo@gmail.com>
2021-07-20 23:59:24 +0200

    working on database connection

--- server/config/key.js
+++ server/config/key.js
@@ -1,4 +1,4 @@
 module.exports = {
-    ATLAS_URI: 'mongodb+srv://YannickNjilo:Jocker237@cluster0.bwiea.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    ATLAS_URI: 'mongodb+srv://YannickNjilo:Jocker237@cluster0.bwiea.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     PORT: '5000'
 }
